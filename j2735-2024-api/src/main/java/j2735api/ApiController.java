e620cd6f1179783fcba9d65011fac931d67ee4a1
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-10-08 00:36:20 -0600

    Remove unused stuff

--- j2735-2024-api/src/main/java/j2735api/ApiController.java
+++ j2735-2024-api/src/main/java/j2735api/ApiController.java
@@ -9,14 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import j2735ffm.MessageFrameCodec;
-import us.dot.its.jpo.ode.api.models.messages.TimestampedMessageFrame;
-import us.dot.its.jpo.ode.api.models.messages.TimestampedMessageFrameList;
 
 import java.io.IOException;
-import java.lang.foreign.Arena;
-import java.lang.foreign.MemorySegment;
 import java.util.Base64;
-import java.util.Formatter;
 import java.util.HexFormat;
 
 import static org.springframework.http.MediaType.*;
@@ -60,23 +55,9 @@ public class ApiController {
         return HexFormat.of().formatHex(bytes);
     }
 
-    @PostMapping(
-            value = "/jer/uper/bin",
-            consumes = APPLICATION_JSON_VALUE,
-            produces = APPLICATION_OCTET_STREAM_VALUE)
-    public byte[] jerToUper(@RequestBody String jer) {
-        return codec.jerToUper(jer);
-    }
 
-    @PostMapping(
-            value = "jer/uper/hex",
-            consumes = APPLICATION_JSON_VALUE,
-            produces = TEXT_PLAIN_VALUE
-    )
-    public String jerToUperHex(@RequestBody String jer) {
-        byte[] bytes = codec.jerToUper(jer);
-        return HexFormat.of().formatHex(bytes);
-    }
+
+
 
     @PostMapping(
             value = "/uper/bin/xer",
@@ -113,104 +94,6 @@ public class ApiController {
         return codec.uperToXer(bytes);
     }
 
-    @PostMapping(
-            value = "/uper/bin/jer",
-            consumes = APPLICATION_OCTET_STREAM_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    public String uperToJer(HttpServletRequest request) {
-        try (var is = request.getInputStream()) {
-            byte[] bytes = is.readAllBytes();
-            log.info("Read {} bytes", bytes.length);
-            return codec.uperToJer(bytes);
-        } catch (IOException ioe) {
-            return ioe.getMessage();
-        }
-    }
 
-    @PostMapping(
-            value = "/uper/hex/jer",
-            consumes = TEXT_PLAIN_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    public String uperHexToJer(@RequestBody String uperHex) {
-        byte[] bytes = HexFormat.of().parseHex(uperHex);
-        return codec.uperToJer(bytes);
-    }
 
-    @PostMapping(
-            value = "/uper/b64/jer",
-            consumes = TEXT_PLAIN_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    public String uperBase64ToJer(@RequestBody String base64) {
-        byte[] bytes = base64Decoder.decode(base64);
-        return codec.uperToJer(bytes);
-    }
-
-    @PostMapping(
-            value = "/xer/jer",
-            consumes = APPLICATION_XML_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    public String xerToJer(@RequestBody String xer) {
-        return codec.xerToJer(xer);
-    }
-
-    @PostMapping(
-            value = "/jer/xer",
-            consumes = APPLICATION_JSON_VALUE,
-            produces = APPLICATION_XML_VALUE
-    )
-    public String jerToXer(@RequestBody String jer) {
-        return codec.jerToXer(jer);
-    }
-
-    /**
-     * Convert batch of base64 encoded messages to XER.
-     * <p>Accepts a JSON array with the format:
-     * <pre>
-     * [
-     *     { "timestamp": 1683155399091, "type": "SPAT", "base64": "ABNmIi..." },
-     *     { "timestamp": 1683155410467, "type": "BSM",  "base64": "ABQlCs..."  },
-     *     ...
-     *  ]
-     * </pre>
-     * <p>Returns line-delimited XER in the following format:
-     * <pre>
-     * <![CDATA[
-     *    SPAT,1683155399091
-     *    <MessageFrame><messageId>19</messageId><value><SPAT>...
-     *    BSM,1683155410467
-     *    <MessageFrame><messageId>20</messageId><value><BasicSafetyMessage>...
-     *    ...
-     * ]]>
-     * </pre>
-     * @param messageFrameList A json array of containing base64 messages
-     *
-     * @return line delimited alternating type/timestamps and XML
-     */
-    @PostMapping(
-            value = "/batch/uper/b64/xer",
-            consumes = APPLICATION_JSON_VALUE,
-            produces = TEXT_PLAIN_VALUE
-    )
-    public String batch(@RequestBody TimestampedMessageFrameList messageFrameList) {
-        log.info("Start decoding batch with {} items", messageFrameList.size());
-        Formatter xmlList = new Formatter();
-        try (var arena = Arena.ofConfined()) {
-            MemorySegment messageFrameMemory = arena.allocate(codec.messageFrameAllocateSize);
-            MemorySegment outputBuffer = arena.allocate(codec.textBufferSize);
-            MemorySegment inputBuffer = arena.allocate(codec.uperBufferSize);
-            for (TimestampedMessageFrame messageFrame : messageFrameList) {
-                String xer = codec.uperToXer(messageFrame.getMessageFrame(), arena, messageFrameMemory,
-                        codec.textBufferSize, outputBuffer, inputBuffer);
-                // Line-delimited XML
-                xmlList.format("%s,%s%n", messageFrame.getMessageFrameType(), messageFrame.getTimestamp());
-                xmlList.format("%s%n", xer);
-            }
-        }
-        log.info("Finished decoding batch");
-        return xmlList.toString();
-    }
 }
--- j2735-2024-api/src/main/java/j2735api/J2735ApiApplication.java
+++ j2735-2024-api/src/main/java/j2735api/J2735ApiApplication.java
@@ -1,20 +1,7 @@
 package j2735api;
 
-import j2735ffm.MessageFrameCodec;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.AbstractHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 
 @SpringBootApplication

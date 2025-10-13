f902f9a81965d8aa1d408a6b11c9728e300a81fc
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-10-13 12:04:22 -0600

    Fix export generated files from docker image

--- run-jextract.sh
+++ run-jextract.sh
@@ -3,6 +3,12 @@
 # Echo on
 set -x
 
+# Copy the native library out to the shared volume
+cp /build/out/* /build-lib
+cp -r /build/generated-files/* /generated-files
+
 # Copy the generated Java code to the shared volume
 cp -r /build/java-src/* /generated-jextract
 
+# Keep the container running
+tail -f /dev/null
--- run-lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Echo on
-set -x
-
-# Copy the native library out to the shared volume
-cp /build/out/* /lib
-cp -r /build/generated-files/* /generated-files
-
